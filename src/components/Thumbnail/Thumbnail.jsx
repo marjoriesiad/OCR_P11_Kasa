5ea40303db6db1e953a7965e74dfe04ddccf9216
Marjorie <marjoriesiad@gmail.com>
2022-10-15 23:01:00 +0200

    ajout composent thumbnail

---
+++ src/components/Thumbnail/Thumbnail.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const Thumbnail = ({link, cover, title}) => {
+    return(
+        <article className="card">
+            <Link to={link} className="card__link">
+                <img className="card__img" src={cover} alt={title} />
+                <div className="card__title">
+                    <h3>{title}</h3>
+                </div>
+            </Link>
+        </article>
+    )
+}
+
+export default Thumbnail;
