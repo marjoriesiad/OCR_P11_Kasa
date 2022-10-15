f7280cb2350d2db59e98c6a84fd059067fbc43fd
Marjorie <marjoriesiad@gmail.com>
2022-10-15 23:05:22 +0200

    ajout des logements sur la home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
+import Thumbnail from "../components/Thumbnail/Thumbnail";
 import Footer from "../components/Footer/Footer";
 import { housing } from "../data/housing";
 import bannerHome from "../assets/banner_home.png";
@@ -13,6 +14,11 @@ const Home = () => {
             </header>
             <main>
                 <Banner image={bannerHome} alt="Paysage de montagne" title="Chez vous, partout et ailleurs" />
+                <div className="gallery">
+                    {housing.map((housing) => (
+                        <Thumbnail key={housing.id} link={`/property/${housing.id}`} title={housing.title} cover={housing.cover} />
+                    ))}
+                </div>
             </main>
             <Footer />
         </>
