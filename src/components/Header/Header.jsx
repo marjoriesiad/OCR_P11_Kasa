d4b4e37c7bf2b8d5ffd959ebd1c4705acb23aa45
Marjorie <marjoriesiad@gmail.com>
2022-10-15 22:26:37 +0200

    création du component header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import Logo from "../../assets/logo.svg";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img className="header__logo" src={Logo} alt="Logo Kasa" />
+            <div className="header__navbar">
+                <NavLink to="/">
+                    <h3>Accueil</h3>
+                </NavLink>
+                <NavLink to="/">
+                    <h3>A propos</h3>
+                </NavLink>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
