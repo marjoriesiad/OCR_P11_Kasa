697106d40ab0861bbb7de20812ad5d27a415365f
Marjorie <marjoriesiad@gmail.com>
2022-10-16 06:54:27 +0200

    mise en page du footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import logoFooter from "../../assets/logo-white.svg";
 const Footer = () => {
     return(
         <footer className="footer">
-            <div className="footer__img">
+            <div className="footer__image">
                 <img src={logoFooter} alt="Logo Kasa" />
             </div>
             <p className="footer__copyright">© 2022 Kasa. All rights reserved</p>
